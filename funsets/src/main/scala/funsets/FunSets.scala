c1af5672e005553b370458d1bc420b647f5764bc
nehcgnem <35271275+nehcgnem@users.noreply.github.com>
2019-05-26 17:43:54 -0400

    week 2 done
    week 2

--- funsets/src/main/scala/funsets/FunSets.scala
+++ funsets/src/main/scala/funsets/FunSets.scala
@@ -21,31 +21,28 @@ object FunSets {
   /**
    * Returns the set of the one given element.
    */
-    def singletonSet(elem: Int): Set =
-      (x: Int) => x == elem
-
+    def singletonSet(elem: Int): Set = x => x == elem
   /**
    * Returns the union of the two given sets,
    * the sets of all elements that are in either `s` or `t`.
    */
-    def union(s: Set, t: Set): Set = ???
-  
+    def union(s: Set, t: Set): Set =  x =>  contains(s,x) || contains(t,x)
   /**
    * Returns the intersection of the two given sets,
    * the set of all elements that are both in `s` and `t`.
    */
-    def intersect(s: Set, t: Set): Set = ???
+    def intersect(s: Set, t: Set): Set = x => contains(s,x) && contains(t,x)
   
   /**
    * Returns the difference of the two given sets,
    * the set of all elements of `s` that are not in `t`.
    */
-    def diff(s: Set, t: Set): Set = ???
+    def diff(s: Set, t: Set): Set = x=> contains(s,x) && !contains(t,x)
   
   /**
    * Returns the subset of `s` for which `p` holds.
    */
-    def filter(s: Set, p: Int => Boolean): Set = ???
+    def filter(s: Set, p: Int => Boolean): Set =  intersect(s,p)
   
 
   /**
@@ -58,24 +55,33 @@ object FunSets {
    */
     def forall(s: Set, p: Int => Boolean): Boolean = {
     def iter(a: Int): Boolean = {
-      if (???) ???
-      else if (???) ???
-      else iter(???)
+      if (a>bound) true
+      else if (contains(s,a) && ! contains(p,a)) false
+      else iter(a+1)
     }
-    iter(???)
+    iter(-bound)
   }
-  
+
+
   /**
    * Returns whether there exists a bounded integer within `s`
    * that satisfies `p`.
    */
-    def exists(s: Set, p: Int => Boolean): Boolean = ???
+    def exists(s: Set, p: Int => Boolean): Boolean = !forall(s, x => ! p(x))
   
   /**
    * Returns a set transformed by applying `f` to each element of `s`.
    */
-    def map(s: Set, f: Int => Int): Set = ???
-  
+    def map(s: Set, f: Int => Int): Set = {
+      def iter(a: Int, set: Set, s: Set): Set = {
+        if (a > bound) set
+        else if (contains(s, a)) iter(a + 1, union(set, singletonSet(f(a))), s)
+        else iter(a + 1, set, s)
+
+      }
+      iter(-bound, x=>false, s)
+    }
+
   /**
    * Displays the contents of a set
    */
--- funsets/src/test/scala/funsets/FunSetSuite.scala
+++ funsets/src/test/scala/funsets/FunSetSuite.scala
@@ -43,9 +43,9 @@ class FunSetSuite extends FunSuite {
    * Try it out! Change the values so that the assertion fails, and look at the
    * error message.
    */
-  // test("adding ints") {
-  //   assert(1 + 2 === 3)
-  // }
+   test("adding ints") {
+     assert(1 + 2 === 3)
+   }
 
 
   import FunSets._
@@ -77,6 +77,7 @@ class FunSetSuite extends FunSuite {
     val s1 = singletonSet(1)
     val s2 = singletonSet(2)
     val s3 = singletonSet(3)
+    val s4 = singletonSet(1)
   }
 
   /**
@@ -109,6 +110,16 @@ class FunSetSuite extends FunSuite {
       assert(!contains(s, 3), "Union 3")
     }
   }
+
+  test ("filter"){
+    new TestSets {
+      val s = filter(union(s1,s2), s4)
+      assert(contains(s, 1), "filter 1")
+      assert(!contains(s, 2), "filter 2")
+    }
+  }
+
+
 
 
 }
